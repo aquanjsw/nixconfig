0f70cb094204dd54eea1920cc8c725193ff3bf99
aquanjsw <zhdlcc@gmail.com>
2026-05-16 17:04:04 +0800

    overlay: add commented sing-box v1.14.0

--- overlays.nix
+++ overlays.nix
@@ -1,2 +1,13 @@
 [
+  # (final: prev: {
+  #   sing-box = prev.sing-box.overrideAttrs (old: {
+  #     version = "1.14.0";
+  #     src = prev.fetchFromGithub {
+  #       owner = "SagerNet";
+  #       repo = "sing-box";
+  #       tag = "v1.14.0";
+  #       hash = "";
+  #     };
+  #   });
+  # })
 ]
