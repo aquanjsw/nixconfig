7876184e238cee3ab91327e1b59010d994e694d6
System administrator <root@wsl2.localdomain>
2026-05-26 16:21:25 +0800

    fix: wsl2

--- hosts/wsl2/configuration.nix
+++ hosts/wsl2/configuration.nix
@@ -11,6 +11,9 @@
   wsl.defaultUser = config.user;
 
   networking.hostName = "wsl2";
+  systemd.services.wpa_supplicant.enable = false;
+
+  nixpkgs.hostPlatform = "x86_64-linux";
 
   system.stateVersion = "25.11";
 }
